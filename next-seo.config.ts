0edc7669d3f455b34b384c67da5634c3a1407393
Alaa Zorkane <alaazorkane@gmail.com>
2020-06-14 12:14:14 +0100

    Improve: SEO description

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -3,9 +3,12 @@ import { DefaultSeoProps } from "next-seo";
 const SEO: DefaultSeoProps = {
   title: "Should you use Angular?",
   description:
-    "A website that helps you decide whether to use Angular in your next project or not, backed by facts and science, definitely not a shitpost.",
+    "A website that helps you decide whether to use Angular in your next project or not, backed by facts and science, sponsored by carrot farmers.",
   canonical: "https://angular.alaazorkane.me/",
   openGraph: {
+    title: "Should you use Angular in your next project?",
+    description:
+      "A website that helps you decide whether to use Angular in your next project or not, backed by facts and science, sponsored by carrot farmers.",
     type: "website",
     locale: "en_EN",
     url: "https://angular.alaazorkane.me/",
