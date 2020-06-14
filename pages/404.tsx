6b696f02d7c1fead7f631af12e71bbba46efd80d
Alaa Zorkane <alaazorkane@gmail.com>
2020-06-14 09:58:09 +0100

    Add: custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const ErrorPage: FC = () => {
+  return (
+    <div className="m-auto text-center sm:text-left">
+      <h1 className="text-2xl">404 | 🥕 not found :(</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
